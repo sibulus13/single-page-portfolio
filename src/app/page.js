2a6cbcd1ce3e0ddf6f01457e2c757800750d7221
Sib <chengjie.michael.huang@gmail.com>
2024-07-16 12:52:11 -0700

    feat: Add About component for displaying personal information
    The code changes introduce a new `About` component that displays personal information about the developer. This component provides details about the developer's background, experience, and interests. It enhances the user experience by providing a comprehensive overview of the developer's skills and expertise.

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,26 @@
 import Hero from "@/components/Hero/HeroSection";
+import About from "@/components/About/About";
 
 export default function Home() {
   return (
     <main>
-      <div id="home" style={{ minHeight: '400px' }}>
+      <div id="home" style={{ minHeight: '400px', paddingTop: '10px', paddingBottom: '10px' }}>
         <Hero />
       </div>
-      <div id="about" style={{ minHeight: '400px' }}>
+      <div id="about" style={{ minHeight: '400px', paddingTop: '10px', paddingBottom: '10px' }}>
         <h1>ABOUT</h1>
+        <About />
       </div>
-      <div id="experience" style={{ minHeight: '400px' }}>
+      <div id="experience" style={{ minHeight: '400px', paddingTop: '10px', paddingBottom: '10px' }}>
         <h1>EXPERIENCE</h1>
       </div>
-      <div id="projects" style={{ minHeight: '400px' }}>
+      <div id="projects" style={{ minHeight: '400px', paddingTop: '10px', paddingBottom: '10px' }}>
         <h1>PROJECTS</h1>
       </div>
-      <div id="photography" style={{ minHeight: '400px' }}>
+      <div id="photography" style={{ minHeight: '400px', paddingTop: '10px', paddingBottom: '10px' }}>
         <h1>PHOTOGRAPHY</h1>
       </div>
-      <div id="blog" style={{ minHeight: '400px' }}>
+      <div id="blog" style={{ minHeight: '400px', paddingTop: '10px', paddingBottom: '10px' }}>
         <h1>BLOG</h1>
       </div>
     </main>
---
+++ src/components/About/About.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <p>
+        Back in 2016, I wrote my first program to breath life into an autonomous
+        robot for my Mechatronics degree. Seeing the impact of those lines of
+        code sparked my interest in software development. A few years later,
+        credited with a Bachelors of Applied Science in Mechatronic Systems
+        Engineering with a minor in AI, I was ready to take on the world of
+        tech.
+      </p>
+      <br />
+      <p>
+        Today, I have the privilege of amassing a diverse software development
+        journey, I have completed many project life cycles for my school, a
+        leading global provider of clean energy solutions, a robotic start-up,
+        and a data-driven farm wealth solutions platform.
+      </p>
+      <br />
+      <p>
+        I take pride in building robust software that are both functional and
+        aesthetic. In my free time, I enjoy automating the mundane things, and
+        dabbling in new technology.
+      </p>
+      <br />
+      <p>
+        When I’m not tinkering away at my computer, you can find me fidgeting
+        with photography, exploring the mountains, riding through the twisties,
+        or mastering the ukulele.
+      </p>
+    </div>
+  );
+};
+
+export default About;
