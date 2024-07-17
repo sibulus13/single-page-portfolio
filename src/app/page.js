f039fc3e8b7794d9e7904d2ddb85b5108709ef54
Sib <chengjie.michael.huang@gmail.com>
2024-07-16 23:31:31 -0700

    ```text refactor: Add Projects component to Home page
    The code changes in the Home page component add the Projects component to display a section for showcasing projects. This enhances the user experience by providing a dedicated section to highlight the projects.

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,35 @@
 import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
 import Experience from "@/components/Experience/Experience";
+import Projects from "@/components/Projects/Projects";
 import Photo from "@/components/Photo/Photo";
 
 // TODO remove min-h-400 when ALL contents are added
 export default function Home() {
   return (
     <main>
-      <div id="home" className="min-h-400 py-10">
+      <div id="home" className="py-10">
         <Hero />
       </div>
-      <div id="about" className="min-h-400 py-10">
-        <h1 className="pt-20">ABOUT</h1>
+      <div id="about" className="py-10">
+        <h1>ABOUT</h1>
         <About />
       </div>
-      <div id="experience" className="min-h-400 py-10">
-        <h1 className="pt-20">EXPERIENCE</h1>
+      <div id="experience" className="py-10">
+        <h1>EXPERIENCE</h1>
         <Experience />
       </div>
-      <div id="projects" className="min-h-400 py-10">
-        <h1 className="pt-20">PROJECTS</h1>
+      <div id="projects" className="py-10">
+        <h1>PROJECTS</h1>
+        <Projects content_type={'projects'} />
       </div>
-      <div id="photography" className="min-h-400 py-10">
-        <h1 className="pt-20">PHOTOGRAPHY</h1>
+      <div id="photography" className="py-10">
+        <h1>PHOTOGRAPHY</h1>
         <Photo />
       </div>
-      <div id="blog" className="min-h-400 py-10">
-        <h1 className="pt-20">BLOG</h1>
+      <div id="blog" className="py-10">
+        <h1>BLOG</h1>
+        <Projects content_type={'adventure'} />
       </div>
     </main>
   );
