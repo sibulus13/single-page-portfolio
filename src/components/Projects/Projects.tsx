953334e5646d5eeccd83d095e38cf9a118356911
Sib <chengjie.michael.huang@gmail.com>
2024-07-17 13:29:26 -0700

    refactor: removed unnecessary code

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,14 +1,13 @@
-"use client";
 import React from "react";
-import { useEffect } from "react";
+
 import PostEntry from "../Posts/PostEntry";
 
 export default function Projects({ content }) {
-  content  = content .slice(0, 4); //Grab latest 4 content 
+  content = content.slice(0, 4); //Grab latest 4 content
 
   return (
     <section className="grid gap-4 md:grid-cols-2 md:pt-2">
-      {content .map((project, index) => (
+      {content.map((project, index) => (
         <PostEntry content={project} key={index} />
       ))}
     </section>
