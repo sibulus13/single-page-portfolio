cfb4f1d9a63bcfa92d085f2ecdcb99fbddd6252c
Sib <chengjie.michael.huang@gmail.com>
2024-07-16 23:30:40 -0700

    refactor: Update Hero component to improve readability and accessibility
    The code changes in the Hero component update the structure and content of the component to enhance readability and accessibility. This includes updating the HTML tags, adding appropriate semantic elements, and improving the text hierarchy. These changes improve the overall user experience and make the Hero section more accessible to users with disabilities.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,17 +9,19 @@ const Hero: React.FC = () => {
     <section className="hero">
       <div className="hero-content">
         <div className="grid grid-cols-2 items-center justify-center">
-          <div className="left-column">
+          <div>
             <h1 className="text-3xl font-bold">
               <strong>Michael Huang</strong>
             </h1>
-            <p>Software Engineer</p>
-            <p>Coding a better today 1 line at a time</p>
-            <div className="pt-4">
+            <p className="py-1">Software Engineer</p>
+            <p className="description">
+              Coding a better today 1 line at a time
+            </p>
+            <div className="pt-2">
               <Contacts />
             </div>
           </div>
-          <div className="right-column flex justify-center">
+          <div className="flex justify-center">
             <Image
               src={CartoonFaceshootMask}
               alt="Hero Cartoon Profile Image"
