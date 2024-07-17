ab30768f0e2c8c050210bbaa28da2e3022b48c31
Sib <chengjie.michael.huang@gmail.com>
2024-07-17 00:27:21 -0700

    refactor: Update jsconfig.json and Photo component imports
    The code changes in jsconfig.json update the paths configuration to remove the "@" symbol from the public path. This change aligns with the updated import statements in the Photo component, where the "@" symbol is replaced with "public/". These modifications ensure that the correct paths are used for importing the photo images, improving the overall functionality and maintainability of the codebase.

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import GridGallery from "./GridGallery";
 
-import Img1 from "@public/photos/1.jpg";
-import Img2 from "@public/photos/2.jpg";
-import Img3 from "@public/photos/3.jpg";
-import Img4 from "@public/photos/4.jpg";
-import Img5 from "@public/photos/5.jpg";
-import Img6 from "@public/photos/6.jpg";
+import Img1 from "public/photos/1.jpg";
+import Img2 from "public/photos/2.jpg";
+import Img3 from "public/photos/3.jpg";
+import Img4 from "public/photos/4.jpg";
+import Img5 from "public/photos/5.jpg";
+import Img6 from "public/photos/6.jpg";
 
 const Imgs = [Img1, Img2, Img6, Img4, Img5, Img3];
 const Photo: React.FC = () => {
