146301ea187f1306edc36310ede64cf44c2a746b
Sib <chengjie.michael.huang@gmail.com>
2024-07-17 13:38:26 -0700

    refactor: Update Contentful import in page.js for consistency
    The code changes in page.js update the import statement for the Contentful module to use consistent capitalization. This change ensures that the import matches the file name and improves code readability and maintainability.

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import About from "@/components/About/About";
 import Experience from "@/components/Experience/Experience";
 import Projects from "@/components/Projects/Projects";
 import Photo from "@/components/Photo/Photo";
-import { getBlogPosts } from "@/Contentful/contentful";
+import { getBlogPosts } from "@/Contentful/Contentful";
 
 let projects = await getBlogPosts('projects');
 let blogs = await getBlogPosts('adventure');
