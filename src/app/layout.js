4b7e381bc7fd0304b65adf70812f45584c4a98bf
Sib <chengjie.michael.huang@gmail.com>
2024-07-16 19:31:24 -0700

    refactor: Update body className in RootLayout component for better responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={'w-10/12 flex flex-col mx-auto ' + inter.className} >
+      <body className={'w-11/12 flex flex-col mx-auto ' + inter.className} >
         <NavBar />
         {children}
       </body>
