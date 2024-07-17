c0a29e26771eeb70ec469819952881d6209361fd
Sib <chengjie.michael.huang@gmail.com>
2024-07-16 23:29:26 -0700

    ```text refactor: Update Footer component to include copyright information
    The code changes in the Footer component add the copyright information to the footer section of the website. This update enhances the completeness and professionalism of the website by providing the necessary legal information.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+import { FaHome } from "react-icons/fa";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="py-6 flex justify-evenly items-center">
+        <Link href="#home" className="text-2xl">
+          <FaHome />
+        </Link>
+        <div className="flex flex-col items-center">
+          <p className="description">Made in Canada</p>
+          <p className="description">© 2024 Michael Huang</p>
+        </div>
+        {/* TODO Implement get in touch animation & logic */}
+        {/* <div>
+          <Link href="">
+            <p className="description">Get in touch</p>
+          </Link>
+        </div> */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
