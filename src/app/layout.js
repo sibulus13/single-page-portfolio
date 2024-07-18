c6ebff835af7667045627d4f2535f45c0796e27c
Si8n <chengjie.michael.huang@gmail.com>
2024-07-18 16:32:20 -0700

    Incoporated vercel speedinsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 import Header from "@/components/Header";
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html >
