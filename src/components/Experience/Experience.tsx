018199a337c28d36af6770296d4b3b1b6dd91085
Si8n <chengjie.michael.huang@gmail.com>
2024-07-18 14:20:09 -0700

    Added hover effect for experiences

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -64,9 +64,9 @@ const experiences = [
 
 const Experience: React.FC = () => {
   return (
-    <div className="grid gap-8">
+    <div className="grid gap-4 py-8">
       {experiences.map((experience, index) => (
-        <div key={experience.title} className="experience peer border p-8">
+        <div key={experience.title} className="experience -mx-4">
           <div>
             <p className="date">{experience.date}</p>
             <span>
