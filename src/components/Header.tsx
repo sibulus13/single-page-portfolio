a767f5fdc809fae2ffc03ad536f76012fa00496d
Si8n <chengjie.michael.huang@gmail.com>
2024-07-18 15:55:13 -0700

    Added black top background to make header navbar hide scrolled over content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,34 +21,37 @@ const Header: React.FC = () => {
   const pathname = usePathname();
   const isHomePg = pathname === "/";
   return (
-    <nav className="sticky top-3 p-2 rounded-3xl backdrop-filter backdrop-blur-3xl text-xs md:text-lg z-10">
-      {isHomePg ? (
-        <ul className="flex justify-between items-center">
-          {items.map((item) => (
-            <li key={item.href}>
-              {item.name === "Home" ? (
-                <Link href={item.href} className="md:text-2xl">
-                  <FaHome />
-                </Link>
-              ) : (
-                <Link className="hover:text-fuchsia-800" href={item.href}>
-                  {item.name}
-                </Link>
-              )}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div className="flex justify-between">
-          <Link href={"/"} className="md:text-2xl">
-            <FaHome />
-          </Link>
-          <button onClick={router.back}>
-            <FaAngleLeft />
-          </button>
-        </div>
-      )}
-    </nav>
+    <div className="sticky top-0 -mx-9 z-10 flex flex-col">
+      <div className="bg-black p-2"></div>
+      <nav className=" p-2 rounded-full backdrop-filter backdrop-blur-3xl text-xs md:text-lg">
+        {isHomePg ? (
+          <ul className="flex justify-between items-center p-2">
+            {items.map((item) => (
+              <li key={item.href}>
+                {item.name === "Home" ? (
+                  <Link href={item.href} className="md:text-2xl">
+                    <FaHome />
+                  </Link>
+                ) : (
+                  <Link className="hover:text-fuchsia-800" href={item.href}>
+                    {item.name}
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="flex justify-between">
+            <Link href={"/"} className="md:text-2xl">
+              <FaHome />
+            </Link>
+            <button onClick={router.back}>
+              <FaAngleLeft />
+            </button>
+          </div>
+        )}
+      </nav>
+    </div>
   );
 };
 
