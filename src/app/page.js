e1cf9c4ddfb2f5ec6fdc6e9c3c19f77b1d282236
Sib <chengjie.michael.huang@gmail.com>
2024-07-23 13:12:22 -0700

    refactor: improved padding for scrollability per breakpoint

--- src/app/page.js
+++ src/app/page.js
@@ -17,23 +17,23 @@ export default function Home() {
         <Hero />
       </div>
       <div id="about">
-        <h1 className="pt-24">ABOUT</h1>
+        <h1 className="pt-20 lg:pt-24">ABOUT</h1>
         <About />
       </div>
       <div id="experience">
-        <h1 className="pt-24">EXPERIENCE</h1>
+        <h1 className="pt-20 lg:pt-24">EXPERIENCE</h1>
         <Experience />
       </div>
       <div id="projects">
-        <h1 className="pt-24">PROJECTS</h1>
+        <h1 className="pt-20 lg:pt-24">PROJECTS</h1>
         <Projects content={projects} type={'Projects'} />
       </div>
       <div id="photography">
-        <h1 className="pt-24">PHOTOGRAPHY</h1>
+        <h1 className="pt-20 lg:pt-24">PHOTOGRAPHY</h1>
         <Photo />
       </div>
       <div id="blog">
-        <h1 className="pt-24">BLOG</h1>
+        <h1 className="pt-20 lg:pt-24">BLOG</h1>
         <Projects content={blogs} type={'Blog'} />
       </div>
     </main>
