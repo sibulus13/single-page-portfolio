be2a1d5611ec77ef9482e394effc2445965398c0
Sib <chengjie.michael.huang@gmail.com>
2024-07-23 03:22:14 -0700

    refactor: projectKey working

--- src/OpenReplay/OpenReplay.tsx
+++ src/OpenReplay/OpenReplay.tsx
@@ -1,7 +1,6 @@
 import Tracker from "@openreplay/tracker";
-import { env } from "process";
 
 export const tracker = new Tracker({
-  projectKey: env.NEXT_PUBLIC_OPEN_REPLAY_PROJECT_KEY,
+  projectKey: process.env.NEXT_PUBLIC_OPEN_REPLAY_PROJECT_KEY,
   __DISABLE_SECURE_MODE: true,
 });
