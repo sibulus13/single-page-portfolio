61f125ab739b0959b9a102e64cf00bf55772bfc2
Sib <chengjie.michael.huang@gmail.com>
2024-07-25 13:40:38 -0700

    spaced the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { FaArrowUp } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer>
-      <div className="py-6 flex justify-evenly items-center">
+      <div className="py-6 mt-10 flex justify-evenly items-center border-t-2">
         <Link href="#home" className="text-2xl clickable">
           <FaArrowUp />
         </Link>
