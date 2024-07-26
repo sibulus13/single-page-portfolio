6fa7eae7caa660058e77ca6cb0e641c4e90417ff
Sib <chengjie.michael.huang@gmail.com>
2024-07-25 21:33:22 -0700

    Added see more to photo section

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import GridGallery from "./GridGallery";
 
@@ -10,9 +11,16 @@ import Img6 from "public/Photos/6.jpg";
 
 const Imgs = [Img1, Img2, Img6, Img4, Img5, Img3];
 const Photo = () => {
+  const [photos, setPhotos] = React.useState(Imgs.slice(0, 3));
+
+  const handleSeeMore = () => {
+    const morePhotos = Imgs.slice(photos.length, photos.length + 3);
+    setPhotos([...photos, ...morePhotos]);
+  }
+
   return (
     <div>
-      {Imgs.map((img, index) => {
+      {photos.map((img, index) => {
         if ((index + 1) % 3 === 0) {
           return (
             <div key={index}>
@@ -27,6 +35,13 @@ const Photo = () => {
         }
         return null;
       })}
+      {photos.length < Imgs.length && (
+        <div className="pt-4">
+          <button onClick={handleSeeMore} className="clickable font-medium">
+            See More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
