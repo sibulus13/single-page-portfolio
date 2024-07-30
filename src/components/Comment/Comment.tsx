484926cf1b1596527d4016edf7c5e891bf902d9d
Sib <chengjie.michael.huang@gmail.com>
2024-07-29 20:12:00 -0700

    Readjusted meme margin

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Comment() {
   return (
     <div>
-      <div className="grid relative aspect-square justify-center m-8">
+      <div className="grid relative aspect-square justify-center m-8 md:mx-20 xl:mx-80">
         <Image
           src="/weary traveler meme.png"
           alt="Weary traveler meme: 'you have scrolled far, weary traveler. Rest, and leave a comment below."
